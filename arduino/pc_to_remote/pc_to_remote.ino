58d7c9f84179ab91cacde78b6546e911c3119dd2
Troels Bang Jensen <thinkofthekittens@gmail.com>
2012-04-27 18:24:37 +0200

    Serial comm tested and working

--- arduino/pc_to_remote/pc_to_remote.ino
+++ arduino/pc_to_remote/pc_to_remote.ino
@@ -8,56 +8,51 @@ void setup(){
   pinMode(TXPIN, OUTPUT);
   pinMode(13,OUTPUT);
 }
-byte x, y;
+int x, y;
 byte incoming;
-byte commandSent = 0;
+byte cmd[] = {
+  0,0};
 byte foo = 0;
 
 void loop(){
   if(Serial.available() > 0){
     incoming = Serial.read();
-    if(commandSent != 0){
-      if(commandSent == 'x'){
-        x = incoming;  
-      }
-      else if(commandSent == 'y'){
-        y = incoming;
-      }
-      commandSent = 0;
-    }else{
-      switch(incoming){
-        case 'x':
-          commandSent = 'x';
-        break;
-        case 'y':
-          commandSent = 'y';
-        break;
+    if(cmd[0] == 't'){
+      if(cmd[1] == 'x'){
+        x = incoming - 127;  
       } 
+      else if(cmd[1] == 'y'){
+        y = incoming;
+        Serial.print("y:");
+        Serial.println(y);
+      }
     }
+    cmd[0] = cmd[1];
+    cmd[1] = incoming; 
   }
   //analogWrite(13, y);
   digitalWrite(13,HIGH);
-  delay(y+128);
+  delayMicroseconds(y);
   digitalWrite(13,LOW);
-  delay(y+128);
-//  sendCmd(10);
+  delayMicroseconds(128-y);
+  //  sendCmd(10);
 }
 
 /*
   Commands:
-  4 end code
-  10 fwd
-  16 fwd+turbo
-  22 Turbo
-  28 Turbo+fwd+left
-  34 Turbo+fwd+right
-  40 back
-  46 back+right
-  52 back+left
-  58 Left
-  64 Right
-  
-*/
+ 4 end code
+ 10 fwd
+ 16 fwd+turbo
+ 22 Turbo
+ 28 Turbo+fwd+left
+ 34 Turbo+fwd+right
+ 40 back
+ 46 back+right
+ 52 back+left
+ 58 Left
+ 64 Right
+ 
+ */
 
 void sendCmd(int cmd){
   w2(4);
@@ -77,10 +72,11 @@ void w1(int n){
 }
 
 void w2(int n){
- for(int i=0;i<n;i++){
+  for(int i=0;i<n;i++){
     digitalWrite(TXPIN, HIGH);
     delayMicroseconds(1500);
     digitalWrite(TXPIN, LOW);
     delayMicroseconds(500);
   }
 }
+
